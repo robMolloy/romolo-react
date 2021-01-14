9de1e9b666b6ca3bcec8f24085b474fecaddb882
robMolloy <robmolloy@hotmail.co.uk>
2021-01-14 18:36:08 +0000

    styled to material design spacing

--- src/components/generic/text/Br.js
+++ src/components/generic/text/Br.js
@@ -1,8 +1,11 @@
-const Br = () => (
-    <>
-        <br />
-        <br />
-    </>
-);
+import { makeStyles } from "@material-ui/core/styles";
+
+const Br = () => {
+  const classes = makeStyles((theme) => ({
+    root: { height: theme.spacing(1), margin: 0, padding: 0 },
+  }))();
+
+  return <br className={classes.root} />;
+};
 
 export default Br;

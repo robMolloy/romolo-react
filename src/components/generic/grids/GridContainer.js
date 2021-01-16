b41617a7dcd70b58ca08a54d217decf8ae60813d
robMolloy <robmolloy@hotmail.co.uk>
2021-01-16 01:47:17 +0000

    decrease default spacing

--- src/components/generic/grids/GridContainer.js
+++ src/components/generic/grids/GridContainer.js
@@ -3,35 +3,35 @@ import MUIGrid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 
 const GridContainer = (props = {}) => {
-    let children, className, spacing, spacingY;
-    ({
-        children,
-        className = "",
-        spacing = 5,
-        spacingY = spacing,
-        ...props
-    } = props);
+  let children, className, spacing, spacingY;
+  ({
+    children,
+    className = "",
+    spacing = 2,
+    spacingY = spacing,
+    ...props
+  } = props);
 
-    const classes = makeStyles((theme) => ({
-        root: {},
-        spacingY: {
-            "& div.MuiGrid-item": {
-                paddingTop: theme.spacing(spacingY / 2),
-                paddingBottom: theme.spacing(spacingY / 2),
-            },
-        },
-    }))();
+  const classes = makeStyles((theme) => ({
+    root: {},
+    spacingY: {
+      "& div.MuiGrid-item": {
+        paddingTop: theme.spacing(spacingY / 2),
+        paddingBottom: theme.spacing(spacingY / 2),
+      },
+    },
+  }))();
 
-    return (
-        <MUIGrid
-            container
-            className={`${className} ${classes.root} ${classes.spacingY}`}
-            spacing={spacing}
-            {...props}
-        >
-            {children}
-        </MUIGrid>
-    );
+  return (
+    <MUIGrid
+      container
+      className={`${className} ${classes.root} ${classes.spacingY}`}
+      spacing={spacing}
+      {...props}
+    >
+      {children}
+    </MUIGrid>
+  );
 };
 
 export default GridContainer;
